f430b2bd7b0bc7f59312e8cd5bf5aafc7342444c
Jaman-M <jaman.manik1@gmail.comm>
2022-05-09 18:29:15 +0600

    added post

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,15 +13,51 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://dbuser1:3caPnOWM8vpz8Tfb@cluster0.sfbbl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sfbbl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('db connected');
-  // perform actions on the collection object
-  client.close();
-});
+// client.connect(err => {
+//   const collection = client.db("dWareHouse").collection("items");
+//   console.log('db connected');
+//   // perform actions on the collection object
+//   client.close();
+// });
 
+async function run(){
+    try{
+        await client.connect();
+        const userCollection = client.db("dWareGouse").collection("user");
+        // const user = { name: 'bila', email: 'billa@gmail.com'};
+        // const result = await userCollection.insertOne(user);
+        // console.log(`user inserted with id: ${result.insertedId}`)
+        
+        app.get('/item', async(req, res) =>{
+            const query = {};
+            const cursor = userCollection.find(query);
+            const users = await cursor.toArray();
+            res.send(users);
+        });
+
+        app.get('/item/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query={_id: ObjectId(id)};
+            const users = await userCollection.findOne(query);
+            res.send(users);
+        });
+
+        //post
+        app.post('/item', async(req, res) =>{
+            const newUser = req.body;
+            const result = await userCollection.insertOne(newUser);
+            res.send(result);
+        })
+
+    }
+    finally{
+        // await client.close();
+    }
+
+}
+run().catch(console.dir);
 
 app.get('/',(req,res)=>{
     res.send('Running D-Warehouse Server');
